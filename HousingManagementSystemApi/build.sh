8ad77bfa7a1b6556e8abcf840e72a1526337fd4b
maysakanoni <maysa@madetech.com>
2022-03-24 15:55:20 +0000

    update to .net6.0

--- HousingManagementSystemApi/build.sh
+++ HousingManagementSystemApi/build.sh
@@ -8,7 +8,7 @@ then
 fi
 
 #dotnet restore
-dotnet tool install --global Amazon.Lambda.Tools --version 5.0.0
+dotnet tool install --global Amazon.Lambda.Tools --version 6.0.0
 
 
 # (for CI) ensure that the newly-installed tools are on PATH
@@ -18,4 +18,4 @@ then
 fi
 
 dotnet restore
-dotnet lambda package --configuration release --framework net5.0 --output-package ./bin/release/net5.0/HousingManagementSystemApi.zip
+dotnet lambda package --configuration release --framework net6.0 --output-package ./bin/release/net6.0/HousingManagementSystemApi.zip
