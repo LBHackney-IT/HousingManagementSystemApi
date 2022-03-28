c5b2be7804bd59f239d8277582088f05b6cb4636
maysakanoni <maysa@madetech.com>
2022-03-28 11:08:55 +0100

    add environment variables to ssm

---
+++ terraform/development/variables.tf
@@ -0,0 +1,14 @@
+variable "authentication_identifier" {
+    description = "Authentication identifier for api"
+    type        = string
+}
+
+variable "jwt_secret" {
+    description = "Jwt secret for api"
+    type        = string
+}
+
+variable "universal_housing_connection_string" {
+    description = "Connection string for universal housing"
+    type        = string
+}
