8c4271e7a740c48dd6e7d556bf5f5cb6690b0d4c
maysakanoni <maysa@madetech.com>
2022-03-28 12:47:55 +0100

    add terraform for staging and production

---
+++ terraform/staging/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+    region  = "eu-west-2"
+    version = "~> 2.0"
+}
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+
+terraform {
+    backend "s3" {
+        bucket  = "terraform-state-housing-development"
+        encrypt = true
+        region  = "eu-west-2"
+        key     = "services/housing-repairs-online/state"
+    }
+}
+
+
+resource "aws_ssm_parameter" "authentication_identifier" {
+    name  = "/HousingManagementSystemApi/staging/authentication-identifier"
+    type  = "String"
+    value = var.authentication_identifier
+}
+resource "aws_ssm_parameter" "jwt_secret" {
+    name  = "/HousingManagementSystemApi/staging/jwt-secret"
+    type  = "String"
+    value = var.jwt_secret
+}
+resource "aws_ssm_parameter" "universal_housing_connection_string" {
+    name  = "/HousingManagementSystemApi/staging/universal-housing-connection-string"
+    type  = "String"
+    value = var.universal_housing_connection_string
+}
+
