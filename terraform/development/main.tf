e763fcfa269b4d49bb9370d6a48e38ef879757ec
maysakanoni <maysa@madetech.com>
2022-03-28 11:08:55 +0100

    add environment variables to ssm

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -11,19 +11,24 @@ terraform {
         bucket  = "terraform-state-housing-development"
         encrypt = true
         region  = "eu-west-2"
-        key     = "services/housing-repairs-online/state" #e.g. "services/transactions-api/state"
+        key     = "services/housing-repairs-online/state"
     }
 }
 
 
-resource "aws_secretsmanager_secret" "authentication_identifier" {
-    name = "HousingManagementSystemApi/authentication-identifier/development"
+resource "aws_ssm_parameter" "authentication_identifier" {
+    name  = "HousingManagementSystemApi/authentication-identifier/development"
+    type  = "String"
+    value = var.authentication_identifier
+}
+resource "aws_ssm_parameter" "jwt_secret" {
+    name  = "HousingManagementSystemApi/jwt-secret/development"
+    type  = "String"
+    value = var.jwt_secret
+}
+resource "aws_ssm_parameter" "universal_housing_connection_string" {
+    name  = "HousingManagementSystemApi/universal-housing-connection-string/development"
+    type  = "String"
+    value = var.universal_housing_connection_string
 }
 
-resource "aws_secretsmanager_secret" "jwt_secret" {
-    name = "HousingManagementSystemApi/jwt-secret/development"
-}
-
-resource "aws_secretsmanager_secret" "universal_housing_connection_string" {
-    name = "HousingManagementSystemApi/universal-housing-connection-string/development"
-}
