6863774eb8a7dde54ad8e7a2337091457d3f3e46
maysakanoni <maysa@madetech.com>
2022-05-11 14:37:53 +0100

    remove unnecessary code

--- HousingManagementSystemApi/Constants.cs
+++ HousingManagementSystemApi/Constants.cs
@@ -1,9 +1,5 @@
 namespace HousingManagementSystemApi;
 
-using System.Collections.Generic;
-using System.Linq;
-using Hackney.Shared.Asset.Domain;
-
 public static class Constants
 {
     public const string ApiV1RoutePrefix = "api/v1/repairsonline/";
