cb74adcbd0f2f4d1107aef88b62d91c6325ff8af
Callum Macpherson <88662046+LBHCallumM@users.noreply.github.com>
2022-12-07 16:14:07 +0000

    Add logging + refactor (#19)
    * Add logging
    
    * Add logging and refactor
    
    * resolve build
    
    * Update test
    
    * Add more logs
    
    Co-authored-by: Dave Dean <david.dean@hackney.gov.uk>

--- HousingManagementSystemApi/Program.cs
+++ HousingManagementSystemApi/Program.cs
@@ -6,14 +6,35 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace HousingManagementSystemApi
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,6 +43,7 @@ namespace HousingManagementSystemApi
                 {
                     webBuilder.SetupSentry();
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog();
     }
 }
--- HousingManagementSystemApi/UseCases/RetrieveAddressesUseCase.cs
+++ HousingManagementSystemApi/UseCases/RetrieveAddressesUseCase.cs
@@ -7,56 +7,107 @@ using HousingManagementSystemApi.Gateways;
 namespace HousingManagementSystemApi.UseCases
 {
     using System.Linq;
+    using System.Reflection.Metadata.Ecma335;
+    using Amazon.Runtime.Internal.Util;
     using Hackney.Shared.Asset.Domain;
     using Hackney.Shared.Tenure.Domain;
+    using Microsoft.Extensions.Logging;
 
     public class RetrieveAddressesUseCase : IRetrieveAddressesUseCase
     {
-        private readonly IAddressesGateway addressesGateway;
-        private readonly IAssetGateway assetGateway;
-        private readonly IEnumerable<AssetType> assetTypes;
-        private readonly ITenureGateway tenureGateway;
+        private readonly IAddressesGateway _addressesGateway;
+        private readonly IAssetGateway _assetGateway;
+        private readonly IEnumerable<AssetType> _assetTypes;
+        private readonly ITenureGateway _tenureGateway;
+        private readonly ILogger<RetrieveAddressesUseCase> _logger;
 
         private IEnumerable<string> EligibleTenureCodes { get; }
-        public RetrieveAddressesUseCase(IAddressesGateway addressesGateway, IAssetGateway assetGateway,
-            IEnumerable<AssetType> assetTypes, ITenureGateway tenureGateway, IEnumerable<TenureType> eligibleTenureTypes)
+
+        public RetrieveAddressesUseCase(
+            IAddressesGateway addressesGateway,
+            IAssetGateway assetGateway,
+            IEnumerable<AssetType> assetTypes,
+            ITenureGateway tenureGateway,
+            IEnumerable<TenureType> eligibleTenureTypes,
+            ILogger<RetrieveAddressesUseCase> logger)
         {
-            this.addressesGateway = addressesGateway;
-            this.assetGateway = assetGateway;
-            this.assetTypes = assetTypes;
-            this.tenureGateway = tenureGateway;
-            this.EligibleTenureCodes = eligibleTenureTypes.Select(x => x.Code);
+            _addressesGateway = addressesGateway;
+            _assetGateway = assetGateway;
+            _assetTypes = assetTypes;
+            _tenureGateway = tenureGateway;
+            EligibleTenureCodes = eligibleTenureTypes.Select(x => x.Code);
+            _logger = logger;
         }
 
-        public async Task<IEnumerable<PropertyAddress>> Execute(string postcode)
+        public async Task<IEnumerable<PropertyAddress>> Execute(string postCode)
         {
-            if (postcode == null)
-                throw new ArgumentNullException(nameof(postcode));
+            _logger.LogInformation("Calling RetrieveAddressesUseCase for {PostCode}", postCode);
 
-            if (postcode == "")
-                return new List<PropertyAddress>();
-            var result = await addressesGateway.SearchByPostcode(postcode);
+            if (string.IsNullOrWhiteSpace(postCode))
+            {
+                _logger.LogInformation("The value of {PostCode} was null or whitespace. Throwing Exception", postCode);
+                throw new ArgumentNullException(nameof(postCode));
+            }
+
+            var result = await _addressesGateway.SearchByPostcode(postCode);
+
+            _logger.LogInformation("Retrieved {Count} results from addressGateway for {PostCode}", result.Count(), postCode);
+
             var filteredAssets = new List<PropertyAddress>();
 
             await Parallel.ForEachAsync(result, async (property, _) =>
             {
-                var asset = await assetGateway.RetrieveAsset(property.Reference.ID);
-                if (assetTypes.Contains(asset.AssetType))
-                {
-                    if (asset.Tenure != null && asset.Tenure.Id != null)
-                    {
-                        var tenureInformation = await tenureGateway.RetrieveTenureType(asset?.Tenure?.Id);
-                        var tenureTypeCode = tenureInformation?.TenureType?.Code;
+                var filteredAsset = await FilterAsset(property, postCode);
 
-                        if (EligibleTenureCodes.Contains(tenureTypeCode))
-                        {
-                            filteredAssets.Add(property);
-                        }
-                    }
+                if (filteredAsset != null)
+                {
+                    filteredAssets.Add(filteredAsset);
                 }
             });
 
+            _logger.LogInformation("Returning {Count} filteredAssets from RetrieveAddressesUseCase for {PostCode}", filteredAssets.Count(), postCode);
+
             return filteredAssets;
+        }
+
+        private async Task<PropertyAddress> FilterAsset(PropertyAddress property, string postCode)
+        {
+            var asset = await _assetGateway.RetrieveAsset(property.Reference.ID);
+
+            if (asset == null)
+            {
+                _logger.LogInformation("The asset with {Id} returned null for postCode {PostCode}. Skipping iteration", property.Reference.ID, postCode);
+                return null;
+            }
+
+            if (!_assetTypes.Contains(asset.AssetType))
+            {
+                _logger.LogInformation("The asset with {Id} was skipped because the assetType was {AssetType} for postCode {PostCode}. Skipping iteration", property.Reference.ID, asset.AssetType, postCode);
+                return null;
+            }
+
+            if (asset.Tenure == null || asset.Tenure.Id == null)
+            {
+                _logger.LogInformation("Tenure or TenureId was null for postCode {PostCode}. Skipping iteration", postCode);
+                return null;
+            }
+
+            var tenureInformation = await _tenureGateway.RetrieveTenureType(asset?.Tenure?.Id);
+            var tenureTypeCode = tenureInformation?.TenureType?.Code;
+
+            if (tenureTypeCode == null)
+            {
+                _logger.LogInformation("TenureTypeCode was null for postCode {PostCode}. Skipping iteration", postCode);
+                return null;
+            }
+
+            if (!EligibleTenureCodes.Contains(tenureTypeCode))
+            {
+                _logger.LogInformation("The asset was skipped because tenureTypeCode was {TenureTypeCode} for postCode {PostCode}. Skipping iteration", tenureTypeCode, postCode);
+                return null;
+            }
+
+            return property;
         }
     }
 }
