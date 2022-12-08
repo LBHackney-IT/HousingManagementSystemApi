c2f5a98d63658da90896bada56bc813e1f1a8c73
Dave Dean <david.dean@hackney.gov.uk>
2022-12-08 09:39:54 +0000

    Add logging to lambda init (#20)
    * Add logging to lambda init
    
    * reorder usings
    
    * df

--- HousingManagementSystemApi/LambdaEntryPoint.cs
+++ HousingManagementSystemApi/LambdaEntryPoint.cs
@@ -1,6 +1,9 @@
 using System.Diagnostics.CodeAnalysis;
 using Amazon.Lambda.AspNetCoreServer;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
 
 namespace HousingManagementSystemApi
 {
@@ -9,6 +12,13 @@ namespace HousingManagementSystemApi
     {
         protected override void Init(IWebHostBuilder builder)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Console(new CompactJsonFormatter())
+                .CreateLogger();
+
             builder.SetupSentry(true);
             builder
                 .UseStartup<Startup>();
