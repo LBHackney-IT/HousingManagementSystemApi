4e8c20c4f0b95a172877ce90082e48d6a0fb5c94
Dave Dean <david.dean@hackney.gov.uk>
2023-10-23 13:43:55 +0100

    new gateway

--- HousingManagementSystemApi/Gateways/RepairsHubAlertsGateway.cs
+++ HousingManagementSystemApi/Gateways/RepairsHubAlertsGateway.cs
@@ -2,9 +2,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
-using System.Text;
+using System;
 using System.Text.Json;
 using System.Threading.Tasks;
+using HousingManagementSystemApi.Models.RepairsHub;
+using Amazon.Lambda.Core;
 
 namespace HousingManagementSystemApi.Gateways
 {
@@ -17,7 +19,65 @@ namespace HousingManagementSystemApi.Gateways
             _client = httpClientFactory.CreateClient(HttpClientNames.RepairsHubAlerts);
         }
 
-        //[Route("{tenancyAgreementReference}/person-alerts")]
-        //[Route("{propertyReference}/location-alerts")]
+        public async Task<AlertsViewModel> GetPersonAlerts(string tenancyAgreementReference)
+        {
+            if (string.IsNullOrWhiteSpace(tenancyAgreementReference)) throw new ArgumentException(tenancyAgreementReference, nameof(tenancyAgreementReference));
+
+            LambdaLogger.Log("Starting person alert retrieval for tenancy " + tenancyAgreementReference);
+
+            string uri = $"properties/{tenancyAgreementReference}/person-alerts";
+
+            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+
+            LambdaLogger.Log("About to get person alerts at " + uri);
+
+            var response = await _client.SendAsync(request);
+
+            AlertsViewModel alertResult = null;
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                alertResult = await response.Content.ReadFromJsonAsync<AlertsViewModel>();
+
+                LambdaLogger.Log($"Successfully got {alertResult.Alerts.Count} person alerts for tenancy " + tenancyAgreementReference);
+            }
+            else
+            {
+                LambdaLogger.Log($"Call to {uri} was unsuccessful with error {response.StatusCode}, reason: {response.ReasonPhrase}");
+            }
+
+            return alertResult;
+        }
+
+        public async Task<AlertsViewModel> GetLocationAlerts(string propertyReference)
+        {
+            if (string.IsNullOrWhiteSpace(propertyReference))
+                throw new ArgumentException(propertyReference, nameof(propertyReference));
+
+            LambdaLogger.Log("Starting person alert retrieval for location " + propertyReference);
+
+            string uri = $"properties/{propertyReference}/location-alerts";
+
+            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+
+            LambdaLogger.Log("About to get location alerts at " + uri);
+
+            var response = await _client.SendAsync(request);
+
+            AlertsViewModel alertResult = null;
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                alertResult = await response.Content.ReadFromJsonAsync<AlertsViewModel>();
+
+                LambdaLogger.Log($"Successfully got {alertResult.Alerts.Count} location alerts for tenancy " + propertyReference);
+            }
+            else
+            {
+                LambdaLogger.Log($"Call to {uri} was unsuccessful with error {response.StatusCode}, reason: {response.ReasonPhrase}");
+            }
+
+            return alertResult;
+        }
     }
 }
